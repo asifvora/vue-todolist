13ce66f3fb5d9a5db0e5e3b6bca32396e3e894fd
asif <asif.vora@multidots.com>
2019-03-20 16:51:43 +0530

    Header CSS changes.

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="header">
-    <h1>TodoList</h1>
-    <div id="nav">
+    <h1 class="logo-text">TodoList</h1>
+    <div class="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
@@ -16,15 +16,25 @@ export default {
 
 <style scoped>
 .header {
+  height: 60px;
+  width: 100%;
   background: #333;
   color: #fff;
-  text-align: center;
   padding: 10px;
 }
 .header a {
   color: #fff;
   padding-right: 5px;
   text-decoration: none;
+}
+.logo-text {
+  display: inline-block;
+  float: left;
+}
+.nav {
+  display: inline-block;
+  float: right;
+  padding: 10px;
 }
 </style>
 
